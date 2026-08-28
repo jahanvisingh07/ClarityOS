b187426d281fc6cc3a0e738711f521c0b083066e
jsgit07 <workspace0js@gmail.com>
2026-08-28 20:41:08 +0530

    Remove deprecated eslint config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,12 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   typescript: {
-    // Allows production builds to complete even if there are type warnings
     ignoreBuildErrors: true,
-  },
-  eslint: {
-    // Allows production builds to complete even if there are linting warnings
-    ignoreDuringBuilds: true,
   },
 };
 
